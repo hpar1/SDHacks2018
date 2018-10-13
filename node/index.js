d1d3b4b9b9aecc6f1b00ba87b18460951961cb8b
Hamza Parekh <hamza.parekh@gmail.com>
2018-10-13 04:10:12 -0700

    New branch

--- node/index.js
+++ node/index.js
@@ -5,4 +5,5 @@ http.createServer(function (req, res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write("The date and time are currrently: " + dt.myDateTime());
 	res.end('\nHello World!');
+	// added process.env.PORT because Heroku dynamically chooses a port to listen to
 }).listen(process.env.PORT || 8080) // listens for port 8080 and runs when port is accesssed
